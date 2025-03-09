d314f1ab820f3cb4fc4e4b1e31c3e2f108bef78e
Bilal Saeed <bilalsaeedhb@gmail.com>
2025-03-09 13:25:36 +0500

    Updated prompts

--- app.py
+++ app.py
@@ -46,15 +46,15 @@ def evaluate_job(job, profile_details):
     Job Details: {json.dumps(job, indent=2)}
 
     Output must be in json format and must have following info:
-    1. Relevancy of the job (High, Medium, Low)
-    2. Summary 
-    3. Key Points of the job
+    1. Relevancy of the job (High, Medium, Low, Irrelevant)
+    2. Brief Summary about the job
+    3. Important key points about the job such as based on my skills and abilities should I apply for this job or not
     Example:
-    {{
+    "{
         "relevancy": "High",
         "summary": "This job is really interesting and relevant. Client is looking for a Full stack developer .....",
         "keyPoints": {"Keypoint1", "Keypoint2", "Keypoint3"}
-    }}
+    }"
     """
     
     response = ollama.generate(model=MODEL_NAME, prompt=prompt, format=JobEvaluation.model_json_schema())
@@ -65,19 +65,14 @@ def evaluate_job(job, profile_details):
 # Function to generate an ideal proposal
 def generate_proposal(job, profile_details):
     prompt = f"""
-    Generate an ideal proposal for the following job based on my skills and abilities:
+    Generate a detailed proposal for the following job based on my skills and abilities:
     My Details: {json.dumps(profile_details, indent=2)}
-    Job Details: {json.dumps(job, indent=2)}
+    Job to apply for: {json.dumps(job, indent=2)}
 
     Proposal should be in json format and must be covering following details:
     1. Talk about project details
     2. Ask relevant questions
     3. Propose technologies relevant to the job and my skills
-
-    Example:
-    {{
-        "proposal": "Hi Eric! I see you are looking for a Fullstack developer. Let's have a chat about the project details..."
-    }}
     """
     
     response = ollama.generate(model=MODEL_NAME, prompt=prompt, format=Proposal.model_json_schema())
